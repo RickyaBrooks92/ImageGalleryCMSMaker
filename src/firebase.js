2f5de5a709960d7b7bec98f41c8c3d0ed774d3a4
Ricky Brooks <rickyabrooks92@gmail.com>
2024-07-11 18:32:59 -0400

    Deploy React app to GitHub Pages

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoxvtzFDcQ-YlaW4LC5PNM76F_GvE7BYE",
-  authDomain: "rickytickycmsgallery.firebaseapp.com",
-  projectId: "rickytickycmsgallery",
-  storageBucket: "rickytickycmsgallery.appspot.com",
-  messagingSenderId: "384917795352",
-  appId: "1:384917795352:web:d36710baccf2b4f32e096d",
-  measurementId: "G-Z94MNF31TY",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
