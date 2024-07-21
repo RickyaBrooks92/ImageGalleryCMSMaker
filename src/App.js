5b32f5354e54d6173f7d4f9c36dc473605db49d5
Ricky Brooks <rickyabrooks92@gmail.com>
2024-07-21 03:42:09 -0400

    changed the sidebar position to the left side

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HomePage from "./components/HomePage";
+import "./css/HomePage.css";
 
 const App = () => {
   return (
